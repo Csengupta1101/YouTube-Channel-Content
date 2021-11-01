6184275c081365d2a373c92ed6b92af8dc16779f
Chandan Sengupta <75371369+Csengupta1101@users.noreply.github.com>
2021-11-02 00:14:54 +0530

    Add files via upload

---
+++ Star Pattern.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+
+#we take i as 0 primariliy
+#first loop , i =0 , 0+1 =1 *
+#second loop . i =1 , 1+1 =2 **
+#third loop . i =1 , 2+1 =3 ***
+#fourth loop . i =1 , 3+1 =4 ****
+i = 0
+for i in range(4):
+    print('*'*(i+1))
+
+
+# In[ ]:
+
+
+
+
